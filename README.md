aedb21852dfca70a26b1b2fc22a68f452e484aa6
Linette V Maliakal <lmaliakal21@gmail.com>
2020-02-22 16:25:35 -0600

    adding UML to README
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # COMP 373 - Facility of the Jollibee Food Chain
-
+## What It Is
 Project 1 for COMP 373. Building a Facility structure for the Jollibee Fast Food Chain. 
+## UML Diagram
+![UML Diagram](UML.png)
+
--- src/com/store/model/customer/Customer.java
+++ src/com/store/model/customer/Customer.java
@@ -1,5 +1,22 @@
 package com.store.model.customer;
 
 public class Customer {
+	
+	public String customerName;
+	public Long phoneNumber;
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
 
 }

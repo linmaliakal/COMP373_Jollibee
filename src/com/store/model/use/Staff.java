1ff963d328bf281f535f9e252315981ecd9e0704
Linette V Maliakal <lmaliakal21@gmail.com>
2020-02-22 19:34:55 -0600

    added logic for Staff
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- src/com/store/model/use/Staff.java
+++ src/com/store/model/use/Staff.java
@@ -1,5 +1,74 @@
 package com.store.model.use;
 
-public class Staff {
+import java.util.ArrayList;
+import java.util.List;
 
+//import other class packages needed
+
+public class Staff {
+	
+	private long staffSSN;
+	private String name;
+	private String birthdate;
+	private String address;
+	private Long phoneNumber;
+	private String position;
+	//private List<> allStaff = new ArrayList<>();
+	
+	public Staff(long staffSSN, String name, String birthdate, String address, Long phoneNumber, String position) {
+		this.staffSSN = staffSSN;
+		this.name = name;
+		this.birthdate = birthdate;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.position = position;
+	}
+
+	public long getStaffSSN() {
+		return staffSSN;
+	}
+
+	public void setStaffSSN(long staffSSN) {
+		this.staffSSN = staffSSN;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
 }
---
+++ src/com/store/model/use/StaffLogic.java
@@ -0,0 +1,22 @@
+package com.store.model.use;
+
+import java.util.HashSet;
+
+//import com.store.dal;
+
+public class StaffLogic {
+	
+	public Staff getStaff(Long staffSSN) {
+		StaffDAO sd = new StaffDAO();
+		Staff staff = sd.getStaffByID(staffSSN);
+		return staff;
+	}
+	
+	public HashSet<Staff> getAllStaff() {
+		StaffDAO pd = new StaffDAO();
+		HashSet<Staff> allStaff = new HashSet<Staff>();
+		allStaff = pd.getAllStaff();
+		return allStaff;
+	}
+
+}

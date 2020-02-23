fd4d5990d82139597556065ab249815e190002cf
Linette V Maliakal <lmaliakal21@gmail.com>
2020-02-22 23:51:53 -0600

    added TODOs for Client to run tests
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- src/com/store/view/StoreClient.java
+++ src/com/store/view/StoreClient.java
@@ -18,6 +18,11 @@ public class StoreClient {
 		staffPerson.setPhoneNumber(8474019233l);
 		staffPerson.setPosition("Manager");
 		
+		//TODO add new Facility
+		//TODO add Facility Details
+		//TODO add Maintenance
+		//TODO add Maintenance Order for Facility
+		
 		System.out.println("\tName: \t\t\t" + staffPerson.getName() + "\n" + "\tBirthdate: \t\t"+ staffPerson.getBirthdate() + "\n"+ "\tAddress: \t\t" + staffPerson.getAddress() + "\n"+ "\tPhone Number: \t\t" + staffPerson.getPhoneNumber() + "\n" + "\tPosition: \t\t"+ staffPerson.getPosition() + "\n");
 	}
 

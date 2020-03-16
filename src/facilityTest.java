7fa7df7cac619e7063c332d0f3297fe32a6bbd42
macabrera9 <mcabrera2@luc.edu>
2020-03-15 20:52:09 -0500

    Start Unit Tests

--- src/facilityTest.java
+++ src/facilityTest.java
@@ -4,10 +4,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class facilityTest {
+import com.store.model.facility.Facility;
 
+class facilityTest {
+	
+		Facility facility = new Facility(0);
 	@BeforeEach
 	void setUp() throws Exception {
+		
+		facility.getFacilityList();
+		facility.setFacilityId(1);
 	}
 
 	@AfterEach

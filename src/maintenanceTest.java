530153e91756a80d2c927975495a028a35b6b649
Linette V Maliakal <lmaliakal21@gmail.com>
2020-03-21 19:58:31 -0500

    updated Maintenance unit tests
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- src/maintenanceTest.java
+++ src/maintenanceTest.java
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.store.model.maintenance.Maintenance;
+//import com.store.model.maintenance.Order;
+
 class maintenanceTest {
+	
+	Maintenance maintenance = new Maintenance(0);
 
 	@BeforeEach
 	void setUp() throws Exception {
+		maintenance.getOrderId();
+		maintenance.setOrderId(1);
 	}
 
 	@AfterEach
@@ -17,6 +24,11 @@ class maintenanceTest {
 	@Test
 	void test() {
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testMakeFacilityMaintRequest() {
+		maintenance.makeFacilityMaintRequest(1);
 	}
 
 }
